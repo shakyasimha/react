0c775af4c4557f5d94dcf93c1cbe97e538c0c757
shakyasimha <sushakya235@gmail.com>
2023-12-21 09:24:51 +0545

    updated css on business card

--- business-card/src/App.jsx
+++ business-card/src/App.jsx
@@ -2,16 +2,29 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import BusinessCard from './components/Card.jsx'
+import './styles/Card.css'
+//import BusinessCard from './components/Card.jsx'
+
+function BusinessCard(props) {
+  return (
+    <div className='cardbox'>
+      <div className='cardbox--nameholder'>
+        {props.name}
+      </div>
+      
+      <div className='cardbox--designation'>
+        {props.designation}
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <>
       <BusinessCard
-        person = {{
-          name: "Sushovan Shakya",
-          designation: "Electronics Engineer"
-        }}
+        name="Sushovan Shakya"
+        designation="Electronics Engineer"
       />
     </>
   )
